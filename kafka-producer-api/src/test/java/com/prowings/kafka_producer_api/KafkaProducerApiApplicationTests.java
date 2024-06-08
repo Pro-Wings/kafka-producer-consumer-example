26b51045555bdeac01e30a9c94ee03a02be65d09
JD <jayprowings@gmail.com>
2024-06-08 12:47:25 +0530

    first draft

---
+++ kafka-producer-api/src/test/java/com/prowings/kafka_producer_api/KafkaProducerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prowings.kafka_producer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
